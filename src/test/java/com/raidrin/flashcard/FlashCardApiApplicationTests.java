aaefd00ca23511f76847a6e66c413a32a86ebe1b
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2022-08-20 13:26:52 -0600

    Initial commit

---
+++ src/test/java/com/raidrin/flashcard/FlashCardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raidrin.flashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashCardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
